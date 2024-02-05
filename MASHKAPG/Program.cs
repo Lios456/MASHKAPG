c42207ffa9ebeb8fcca9cc96caeff735b9f7b00f
Lios456 <luis.jerez0075@utc.edu.ec>
2024-02-04 23:26:20 -0500

    login como principal

--- MASHKAPG/Program.cs
+++ MASHKAPG/Program.cs
@@ -11,7 +11,7 @@ namespace MASHKAPG
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new VerClientes(new clases.Usuario()));
+            Application.Run(new Login());
         }
     }
 }
